a371f5330d7ab00e5e971ce22069f9974cd7c08a
Mariogg1 <a18margongon@inspedralbes.cat>
2020-05-11 17:35:40 +0200

    millores per a la pagina

--- JS/registre.js
+++ JS/registre.js
@@ -1,24 +1,56 @@
+function tiene_numeros(texto){
+    var numeros="0123456789";
+    for(i=0; i<texto.length; i++){
+        if (numeros.indexOf(texto.charAt(i),0)!=-1){
+            alert('tiene numeros');
+            return 1;
+        }
+    }
+    return 0;
+}
+
 function register(){
+    let formulari_bien=true;
     let user = document.getElementById("registre_usuari").value;
     let password = document.getElementById("registre_password").value;
     let email = document.getElementById("registre_email").value;
     let nom = document.getElementById("registre_nom").value;
     let primer_cognom = document.getElementById("registre_primer_cognom").value;
     let segon_cognom = document.getElementById("registre_segon_cognom").value;
-    
-    axios.post('PHP/registre.php', {
-        registre_usuari: user,
-        registre_password: password,
-        registre_email: email,
-        registre_nom: nom,
-        registre_primer_cognom: primer_cognom,
-        registre_segon_cognom: segon_cognom
-    }).then(function(response) {
-        if(response.data == "Bien") {
-            console.log("Usuari registrat");
-        } 
-        else {
-            console.log(response.data);
-        }
-      });
+    if (user.length == '' || tiene_numeros(user.length)) {
+        document.getElementById("alerta").style.display="inline";
+        formulari_bien=false;
+    }
+    else if(password.length == ''){
+        formulari_bien=false;
+    }
+    else if(email.length == '' || !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
+        formulari_bien=false;
+    }
+    else if(nom.length == '' || tiene_numeros(nom.length)){
+        formulari_bien=false;
+    }
+    else if(primer_cognom.length == '' || tiene_numeros(primer_cognom.length)){
+        formulari_bien=false;
+    }
+    else if(segon_cognom.length == '' || tiene_numeros(segon_cognom.length)){
+        formulari_bien=false;
+    }
+    if(formulari_bien=true){
+        axios.post('PHP/registre.php', {
+            registre_usuari: user,
+            registre_password: password,
+            registre_email: email,
+            registre_nom: nom,
+            registre_primer_cognom: primer_cognom,
+            registre_segon_cognom: segon_cognom
+        }).then(function(response) {
+            if(response.data == "Bien") {
+                console.log("Usuari registrat");
+            } 
+            else {
+                console.log(response.data);
+            }
+        });
+    }
 }
--- index.php
+++ index.php
@@ -1,102 +1,100 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>COVID19</title>
-    <link rel="stylesheet" type="text/css" href="CSS/index.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-   <script src="JS/registre.js"></script>
-</head>
-<body>
-    <div class="todoHeader">
-            <div>
-                <div class="titulo">
-                    <h1>
-                        Informació actualizada sobre el Covid-19
-                    </h1>
-                </div>
-                <!--Navbar-->
-                <nav class="navbar navbar-expand-lg navbar-light bg-white" style="justify-content: center;">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="header.php">Mapa</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Noticies</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Informació</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Foro</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <!--Final navbar-->
-            <!--Formulario-->
-            <form>
-                <div class="formulario">
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>COVID19</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="JS/registre.js"></script>
+        <link rel="stylesheet" type="text/css" href="CSS/index.css">
+    </head>
+    <body>
+        <header>
+            <div class="todoHeader">
                     <div>
-                        <input type="text" placeholder="Email"  name="userName">
+                        <div class="titulo">
+                            <h1>
+                                Informació actualizada sobre el Covid-19
+                            </h1>
+                        </div>
+                        <!--Navbar-->
+                        <nav class="navbar navbar-expand-lg navbar-light bg-white" style="justify-content: center;">
+                            <ul class="navbar-nav">
+                                    <a class="nav-link" href="index.php">Mapa</a>
+                                    <a class="nav-link" href="#">Noticies</a>
+                                    <a class="nav-link" href="#">Informació</a>
+                                    <a class="nav-link" href="#">Foro</a>
+                            </ul>
+                        </nav>
                     </div>
-                    <div>
-                        <div >
-                            <input type="text" placeholder="Password" name="passwordc">
+                    <!--Final navbar-->
+                    <!--Formulario-->
+                    <form>
+                        <div class="formulario">
+                            <div>
+                                <input type="text" placeholder="User name"  name="user_name">
+                            </div>
+                            <div>
+                                <div >
+                                    <input type="password" placeholder="Password" name="passwordc">
+                                </div>
+                            </div>
+                            <div class="boton">
+                                <button type="submit" id="envia">Envia</button>
+                                <button type="button" data-toggle="modal" data-target="#modalRegistre">Registra't</button>
+                            </div>
+                        </div>
+                    </form>
+                    <!--Final formulario-->
+            </div>
+        </header>
+        <div class="modal fade" id="modalRegistre">
+            <div class="modal-dialog modal-login">
+                <div class="modal-content">
+                    <div class="modal-header">				
+                        <h2 class="modal-title">Registre</h2>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">				
+                        <div class="form-group">
+                            <label>*Usuari</label>
+                            <div class="alert alert-warning" id="alerta" role="alert">
+                                This is a warning alert—check it out!
+                            </div>
+                            <input id ="registre_usuari" type="text" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                            <div class="clearfix">
+                                <label>*Clau</label>
+                            </div>
+                            <input id ="registre_password" type="password" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>*Email</label>
+                            <input id ="registre_email" type="email" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>*Nom</label>
+                            <input id ="registre_nom" type="text" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>*Primer Cognom</label>
+                            <input id ="registre_primer_cognom" type="text" class="form-control" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>*Segon Cognom</label>
+                            <input id ="registre_segon_cognom"type="text" class="form-control" required="required">
                         </div>
                     </div>
-                    <div class="boton">
-                        <button type="submit" id="envia">Envia</button>
-                        <button type="button" data-toggle="modal" data-target="#modalRegistre">Registra't</button>
+                    <div class="modal-footer">
+                            <button data-dismiss="modal" type="submit" class="btn btn-outline-dark pull-right enviar" onclick="register()">Registrar</button>
+                        </div> 
                     </div>
-                </div>
-            </form>
-            <!--Final formulario-->
-    </div>
-    <div class="modal fade" id="modalRegistre">
-        <div class="modal-dialog modal-login">
-            <div class="modal-content">
-                <div class="modal-header">				
-                    <h2 class="modal-title">Registre</h2>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                </div>
-                <div class="modal-body">				
-                    <div class="form-group">
-                        <label>Usuari</label>
-                        <input id ="rUsuari" type="text" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <div class="clearfix">
-                              <label>Clau</label>
-                        </div>
-                        <input id ="rPassword" type="password" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input id ="rEmail" type="text" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <label>Nom</label>
-                        <input id ="rNom" type="text" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <label>Primer Cognom</label>
-                        <input id ="rPCognom" type="text" class="form-control" required="required">
-                    </div>
-                    <div class="form-group">
-                        <label>Segon Cognom</label>
-                        <input id ="rSCognom"type="text" class="form-control" required="required">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                  <button data-dismiss="modal" type="submit" class="btn btn-outline-dark pull-right enviar" onclick="register()">Registrar</button>
                 </div>
             </div>
-          </div>
-    </div>
-</body>
+        </div>
+    </body>
 </html>
